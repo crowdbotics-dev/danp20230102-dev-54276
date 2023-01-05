de6df1c544d0c461355aeb4cb617505f1722163c
Daniel Palacios <daniel80380@gmail.com>
2023-01-05 13:41:13 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -5,6 +5,8 @@ import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 const Untitled3 = ({
   navigation
 }) => {
+  const Test = useSelector(state => state.Test);
+  const hola = useSelector(state => state.hola);
   const Login = useSelector(state => state.Login);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
@@ -14,7 +16,10 @@ const Untitled3 = ({
       flex: 1
     }}><Pressable onPress={() => navigation.navigation.navigate("Untitled2", {
         "saludo": "hola",
-        "test": Login.test
+        "test": Login.test,
+        "chau": hola.chau,
+        "testttt": Test.testttt,
+        "hi": "hello"
       })}><View style={styles.HzhxDFZk}></View></Pressable></ScrollView>
     </SafeAreaView>;
 };
